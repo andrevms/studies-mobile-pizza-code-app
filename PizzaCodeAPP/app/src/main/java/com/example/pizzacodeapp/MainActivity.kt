0773321657b2a9a6ab767afe09e5a471d96c7382
André Vitor <andrevms@hotmail.com>
2021-04-16 11:43:00 -0300

    Menu Icon Logout working

--- PizzaCodeAPP/app/src/main/java/com/example/pizzacodeapp/MainActivity.kt
+++ PizzaCodeAPP/app/src/main/java/com/example/pizzacodeapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.pizzacodeapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        mAuth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actions, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if(id == R.id.logoutButton){
+            mAuth?.signOut()
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }else if(id == R.id.sacola){
+            //TODO
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
