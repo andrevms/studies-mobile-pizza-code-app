39ed17acbbac1c06d8fe2a8d91b00dd98f10616f
André Vitor <andrevms@hotmail.com>
2021-04-27 08:41:42 -0300

    Get Data from realtime database working

--- PizzaCodeAPP/app/src/main/java/com/example/pizzacodeapp/ItensProductsAdapter.kt
+++ PizzaCodeAPP/app/src/main/java/com/example/pizzacodeapp/ItensProductsAdapter.kt
@@ -1,33 +1,64 @@
 package com.example.pizzacodeapp
 
-import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-
-class ItensProductsAdapter (
-    private var ctx: Context,
-    private var produtos: List<ItensProducts>
-        ): BaseAdapter() {
-
-    override fun getCount(): Int = produtos.size
-
-    override fun getItem(position: Int): Any = produtos[position]
-
-    override fun getItemId(position: Int): Long = position.toLong()
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val produto = produtos[position]
-        val row = LayoutInflater.from(ctx).inflate(R.layout.item_produto,
-            parent,false)
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 
 
-        //row.findViewById(R.id.item_photo)
-        //row.findViewById(R.id.item_name)
-        //row.findViewById(R.id.item_description)
-        //row.findViewById(R.id.item_price)
+class ItensProductsAdapter(
+        private val produtos: List<ItensProducts>,
+        private val reference: DatabaseReference?,
+        private val productsSize: Int
+): RecyclerView.Adapter<ItensProductsAdapter.VH>(){
 
-        return row
+    class VH(itemView: View): RecyclerView.ViewHolder(itemView){
+        val txtName: TextView? = itemView.findViewById(R.id.txtTitle)
+        //val txtDescription: TextView = itemView.findViewById(R.id.item_description)
+        //val txtPrice: TextView = itemView.findViewById(R.id.item_price)
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.item_message,parent,false)
+        val vh = VH(v)
+
+        vh.itemView.setOnClickListener{
+            val produto = produtos[vh.adapterPosition]
+            //callback(produto)
+        }
+        return vh
+    }
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        Log.v("I","INIT GET DATABASE")
+        reference!!.child("products")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        var i = 0
+                        for (node1 in dataSnapshot.children) {
+                            Log.v("I", node1.child("nameItem").getValue().toString())
+                            if (position == i) {
+                                Log.v("I","dentro if ${i}")
+                                holder.txtName?.setText(node1.child("nameItem").getValue().toString())
+                            }
+                            i += 1
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {}
+                })
+    }
+
+    override fun getItemCount(): Int {
+        Log.v("I", "Produto tamanho data = ${productsSize}")
+        return productsSize
     }
 }
--- PizzaCodeAPP/app/src/main/java/com/example/pizzacodeapp/MainAdminActivity.kt
+++ PizzaCodeAPP/app/src/main/java/com/example/pizzacodeapp/MainAdminActivity.kt
@@ -1,32 +1,76 @@
 package com.example.pizzacodeapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pizzacodeapp.ItensProductsAdapter.*
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+
 
 class MainAdminActivity : AppCompatActivity() {
 
     private var mAuth: FirebaseAuth? = null
+    private var ref: DatabaseReference = FirebaseDatabase.getInstance().getReference()
+    private var recyclerVH : RecyclerView? = null
+    private var productsSize : Int = 0
+
+    private var products = mutableListOf<ItensProducts>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        mAuth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_admin)
+
+
+        recyclerVH = findViewById(R.id.recyclerViewAdmin)
+        ref.child("products")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        Log.v("I", "Entrando OnDataChange")
+                        productsSize = dataSnapshot.childrenCount.toInt()
+                        Log.v("I", "Pegando o ChidrenCount ${productsSize}")
+                        initRecyclerView()
+                    }
+                    override fun onCancelled(databaseError: DatabaseError) {}
+                })
     }
 
+
+
+    private fun initRecyclerView() {
+       recyclerVH?.adapter = ItensProductsAdapter(products, ref, productsSize)
+       val layoutManager = LinearLayoutManager(this)
+
+        recyclerVH?.layoutManager = layoutManager
+
+    }
+
+    fun loadSizeProducts() {
+
+
+    }
+
+
+    //Funções do Menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.actions_admin, menu)
         return true
     }
 
 
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
         if(id == R.id.logoutButton){
+            Log.v("I", "signOut")
             mAuth?.signOut()
             val intent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(intent)
@@ -40,4 +84,6 @@ class MainAdminActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
+
 }
